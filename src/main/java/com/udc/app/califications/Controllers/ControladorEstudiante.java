096f1b0b3211d4567996c75a471295188d1a32fc
kevin-aspire <kgomezr2@unicartagena.edu.co>
2026-05-13 09:19:31 -0500

    feat: creación vacia del controlador para estudiante - Kevin Gomez

---
+++ src/main/java/com/udc/app/califications/Controllers/ControladorEstudiante.java
@@ -0,0 +1,10 @@
+package com.udc.app.califications.Controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller( "student/")
+@Slf4j
+public class ControladorEstudiante {
+
+}
